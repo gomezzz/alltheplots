43428b5a14b01e8b3a2b108f6d2b8f161023a5a0
Pablo Gómez <contact@pablo-gomez.net>
2025-03-22 18:15:40 +0100

    Add logger utility and tensor conversion function to utils module

--- alltheplots/__init__.py
+++ alltheplots/__init__.py
@@ -1,5 +1,6 @@
 from .plots.plot import plot
+from .utils.logger import logger, set_log_level
 
-__all__ = ["plot"]
+__all__ = ["plot", "logger", "set_log_level"]
 __version__ = "0.1.0"
 __author__ = "Pablo Gómez"
