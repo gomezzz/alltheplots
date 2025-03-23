17ace65f029d71f7b2c35c8d866a7fdc20f26052
Pablo Gómez <contact@pablo-gomez.net>
2025-03-23 17:08:47 +0100

    cleaning up

--- alltheplots/plots/two_dim/overview/__init__.py
+++ alltheplots/plots/two_dim/overview/__init__.py
@@ -1,13 +1,9 @@
 from .heatmap import create_heatmap_plot
 from .contour import create_contour_plot
 from .surface_3d import create_surface_3d_plot
-from .scatter import create_scatter_plot
-from .hist3d import create_hist3d_plot
 
 __all__ = [
     "create_heatmap_plot",
     "create_contour_plot",
     "create_surface_3d_plot",
-    "create_scatter_plot",
-    "create_hist3d_plot",
 ]
