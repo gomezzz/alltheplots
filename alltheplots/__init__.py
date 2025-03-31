24eec803dd53dbebf73e6efde8b011cf2290249b
Pablo Gómez <contact@pablo-gomez.net>
2025-03-31 07:43:04 +0000

    Refactor logging configuration to toggle logger based on ATP_RELEASE_TOGGLE

--- alltheplots/__init__.py
+++ alltheplots/__init__.py
@@ -1,10 +1,28 @@
-from .plots.plot import plot
-from .utils.logger import logger, set_log_level
+import sys
 
+from loguru import logger
+
+from .plots.plot import plot
+from .utils.logger import set_log_level
+
+
+ATP_RELEASE_TOGGLE = True
+
+if ATP_RELEASE_TOGGLE:
+    # Disable alltheplots logger
+    logger.disable("alltheplots")
+else:
+    # Enable alltheplots logger
+    logger.remove()
+    logger.add(
+        sys.stderr,
+        format="<green>{time:HH:mm:ss}</green>|<level>ATP-{level}</level>| <level>{message}</level>",
+        colorize=True,
+        level="INFO",
+    )
 
 __all__ = [
     "plot",
-    "logger",
     "set_log_level",
 ]
 
